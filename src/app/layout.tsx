bd9e27782832179f991328e2da190cbe5d3e178b
Nikhil Sundriya <nikhilchoudhary2508@gmail.com>
2026-03-12 22:35:22 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,7 @@ import dynamic from "next/dynamic";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 import { Suspense } from "react";
 
-const Disclaimer = dynamic(() => import("@/components/ui/overlay/Disclaimer"), {
-  ssr: false,
-});
+const Disclaimer = dynamic(() => import("@/components/ui/overlay/Disclaimer"));
 
 export const metadata: Metadata = {
   title: siteConfig.name,
