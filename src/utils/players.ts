2060f119bc799d54b4ba77f524ec40a41de66c81
Nikhil Sundriya <nikhilchoudhary2508@gmail.com>
2026-03-12 22:09:21 +0530

    Update players.ts

--- src/utils/players.ts
+++ src/utils/players.ts
@@ -1,51 +1,64 @@
 import { PlayersProps } from "@/types";
 
 /**
- * Generates a list of movie players with their respective titles and source URLs.
- * Each player is constructed using the provided movie ID.
- *
- * @param {string | number} id - The ID of the movie to be embedded in the player URLs.
- * @param {number} [startAt] - The start position in seconds to be embedded in the player URLs. Optional.
- * @returns {PlayersProps[]} - An array of objects, each containing
- * the title of the player and the corresponding source URL.
+ * Movie Players
  */
 export const getMoviePlayers = (id: string | number, startAt?: number): PlayersProps[] => {
   return [
+    // CLEAN / LESS ADS (TOP SERVERS)
+
     {
-      title: "VidLink",
-      source: `https://vidlink.pro/movie/${id}?player=jw&primaryColor=006fee&secondaryColor=a2a2a2&iconColor=eefdec&autoplay=false&startAt=${startAt || ""}`,
+      title: "VidKing (Default)",
+      source: `https://www.vidking.net/embed/movie/${id}?color=006fee&autoplay=false`,
       recommended: true,
       fast: true,
-      ads: true,
       resumable: true,
     },
     {
-      title: "VidLink 2",
-      source: `https://vidlink.pro/movie/${id}?primaryColor=006fee&autoplay=false&startAt=${startAt}`,
+      title: "VidSrc 5",
+      source: `https://vidsrc.cc/v3/embed/movie/${id}?autoPlay=false`,
       recommended: true,
       fast: true,
-      ads: true,
-      resumable: true,
     },
     {
-      title: "VidKing",
-      // NOTE: VidKing has a known issue with the `progress` query parameter where it stuck at that timestamp.
-      // Currently, this player can save playback progress but cannot resume from a specific timestamp.
-      // The `progress` parameter is commented out in the source URL until this is resolved.
-      source: `https://www.vidking.net/embed/movie/${id}?color=006fee&autoplay=false`, //&progress=${startAt || ""}`,
-      recommended: true,
+      title: "VidSrc 4",
+      source: `https://vidsrc.cc/v2/embed/movie/${id}?autoPlay=false`,
       fast: true,
-      resumable: true,
+    },
+    {
+      title: "VidSrc 2",
+      source: `https://vidsrc.to/embed/movie/${id}`,
+    },
+    {
+      title: "VidSrc 1",
+      source: `https://vidsrc.xyz/embed/movie/${id}`,
+    },
+
+    // MEDIUM ADS
+
+    {
+      title: "SuperEmbed",
+      source: `https://multiembed.mov/directstream.php?video_id=${id}&tmdb=1`,
+      fast: true,
+      ads: true,
     },
     {
       title: "<Embed>",
       source: `https://embed.su/embed/movie/${id}`,
       ads: true,
     },
+
+    // HEAVY ADS (BACKUP SERVERS)
+
     {
-      title: "SuperEmbed",
-      source: `https://multiembed.mov/directstream.php?video_id=${id}&tmdb=1`,
-      fast: true,
+      title: "VidLink",
+      source: `https://vidlink.pro/movie/${id}?player=jw&primaryColor=006fee&secondaryColor=a2a2a2&iconColor=eefdec&autoplay=false&startAt=${startAt || ""}`,
+      ads: true,
+      resumable: true,
+    },
+    {
+      title: "VidLink 2",
+      source: `https://vidlink.pro/movie/${id}?primaryColor=006fee&autoplay=false&startAt=${startAt}`,
       ads: true,
     },
     {
@@ -61,7 +74,6 @@ export const getMoviePlayers = (id: string | number, startAt?: number): PlayersP
     {
       title: "AutoEmbed 1",
       source: `https://autoembed.co/movie/tmdb/${id}`,
-      fast: true,
       ads: true,
     },
     {
@@ -75,30 +87,8 @@ export const getMoviePlayers = (id: string | number, startAt?: number): PlayersP
       ads: true,
     },
     {
-      title: "VidSrc 1",
-      source: `https://vidsrc.xyz/embed/movie/${id}`,
-      ads: true,
-    },
-    {
-      title: "VidSrc 2",
-      source: `https://vidsrc.to/embed/movie/${id}`,
-      ads: true,
-    },
-    {
       title: "VidSrc 3",
       source: `https://vidsrc.icu/embed/movie/${id}`,
-      ads: true,
-    },
-    {
-      title: "VidSrc 4",
-      source: `https://vidsrc.cc/v2/embed/movie/${id}?autoPlay=false`,
-      ads: true,
-    },
-    {
-      title: "VidSrc 5",
-      source: `https://vidsrc.cc/v3/embed/movie/${id}?autoPlay=false`,
-      recommended: true,
-      fast: true,
       ads: true,
     },
     {
@@ -110,15 +100,7 @@ export const getMoviePlayers = (id: string | number, startAt?: number): PlayersP
 };
 
 /**
- * Generates a list of TV show players with their respective titles and source URLs.
- * Each player is constructed using the provided TV show ID, season, and episode.
- *
- * @param {string | number} id - The ID of the TV show to be embedded in the player URLs.
- * @param {string | number} [season] - The season number of the TV show episode to be embedded.
- * @param {string | number} [episode] - The episode number of the TV show episode to be embedded.
- * @param {number} [startAt] - The start position in seconds to be embedded in the player URLs. Optional.
- * @returns {PlayersProps[]} - An array of objects, each containing
- * the title of the player and the corresponding source URL.
+ * TV Show Players
  */
 export const getTvShowPlayers = (
   id: string | number,
@@ -127,41 +109,58 @@ export const getTvShowPlayers = (
   startAt?: number,
 ): PlayersProps[] => {
   return [
+    // CLEAN SERVERS
+
     {
-      title: "VidLink",
-      source: `https://vidlink.pro/tv/${id}/${season}/${episode}?player=jw&primaryColor=f5a524&secondaryColor=a2a2a2&iconColor=eefdec&autoplay=false&startAt=${startAt || ""}`,
+      title: "VidKing (Default)",
+      source: `https://www.vidking.net/embed/tv/${id}/${season}/${episode}?color=f5a524&autoplay=false`,
       recommended: true,
       fast: true,
-      ads: true,
       resumable: true,
     },
     {
-      title: "VidLink 2",
-      source: `https://vidlink.pro/tv/${id}/${season}/${episode}?primaryColor=f5a524&autoplay=false&startAt=${startAt}`,
+      title: "VidSrc 5",
+      source: `https://vidsrc.cc/v3/embed/tv/${id}/${season}/${episode}?autoPlay=false`,
       recommended: true,
       fast: true,
-      ads: true,
-      resumable: true,
     },
     {
-      title: "VidKing",
-      // NOTE: VidKing has a known issue with the `progress` query parameter where it stuck at that timestamp.
-      // Currently, this player can save playback progress but cannot resume from a specific timestamp.
-      // The `progress` parameter is commented out in the source URL until this is resolved.
-      source: `https://www.vidking.net/embed/tv/${id}/${season}/${episode}?color=f5a524&autoplay=false`, //&progress=${startAt || ""}`,
-      recommended: true,
+      title: "VidSrc 4",
+      source: `https://vidsrc.cc/v2/embed/tv/${id}/${season}/${episode}?autoPlay=false`,
       fast: true,
-      resumable: true,
+    },
+    {
+      title: "VidSrc 2",
+      source: `https://vidsrc.to/embed/tv/${id}/${season}/${episode}`,
+    },
+    {
+      title: "VidSrc 1",
+      source: `https://vidsrc.xyz/embed/tv/${id}/${season}/${episode}`,
+    },
+
+    // MEDIUM ADS
+
+    {
+      title: "SuperEmbed",
+      source: `https://multiembed.mov/directstream.php?video_id=${id}&tmdb=1&s=${season}&e=${episode}`,
+      ads: true,
     },
     {
       title: "<Embed>",
       source: `https://embed.su/embed/tv/${id}/${season}/${episode}`,
       ads: true,
     },
+
+    // HEAVY ADS
+
     {
-      title: "SuperEmbed",
-      source: `https://multiembed.mov/directstream.php?video_id=${id}&tmdb=1&s=${season}&e=${episode}`,
-      fast: true,
+      title: "VidLink",
+      source: `https://vidlink.pro/tv/${id}/${season}/${episode}?player=jw&primaryColor=f5a524&secondaryColor=a2a2a2&iconColor=eefdec&autoplay=false&startAt=${startAt || ""}`,
+      ads: true,
+    },
+    {
+      title: "VidLink 2",
+      source: `https://vidlink.pro/tv/${id}/${season}/${episode}?primaryColor=f5a524&autoplay=false&startAt=${startAt}`,
       ads: true,
     },
     {
@@ -171,13 +170,12 @@ export const getTvShowPlayers = (
     },
     {
       title: "NontonGo",
-      source: `https://www.NontonGo.win/embed/tv/${id}/${season}/${episode}`,
+      source: `https://www.nontongo.win/embed/tv/${id}/${season}/${episode}`,
       ads: true,
     },
     {
       title: "AutoEmbed 1",
       source: `https://autoembed.co/tv/tmdb/${id}-${season}-${episode}`,
-      fast: true,
       ads: true,
     },
     {
@@ -191,30 +189,8 @@ export const getTvShowPlayers = (
       ads: true,
     },
     {
-      title: "VidSrc 1",
-      source: `https://vidsrc.xyz/embed/tv/${id}/${season}/${episode}`,
-      ads: true,
-    },
-    {
-      title: "VidSrc 2",
-      source: `https://vidsrc.to/embed/tv/${id}/${season}/${episode}`,
-      ads: true,
-    },
-    {
       title: "VidSrc 3",
       source: `https://vidsrc.icu/embed/tv/${id}/${season}/${episode}`,
-      ads: true,
-    },
-    {
-      title: "VidSrc 4",
-      source: `https://vidsrc.cc/v2/embed/tv/${id}/${season}/${episode}?autoPlay=false`,
-      ads: true,
-    },
-    {
-      title: "VidSrc 5",
-      source: `https://vidsrc.cc/v3/embed/tv/${id}/${season}/${episode}?autoPlay=false`,
-      recommended: true,
-      fast: true,
       ads: true,
     },
     {
